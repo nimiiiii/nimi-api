75df1e6d97f6dd5ccf59ff6606f4a32481c7ebb8
unknown <n20gaming2000@gmail.com>
2020-09-22 20:02:04 +0800

    use decorators

--- lib/models/model.ship.list.item.ts
+++ lib/models/model.ship.list.item.ts
@@ -2,14 +2,28 @@ import RequestError from "lib/requestError";
 import Model from "./model.base";
 
 export default class ShipListItem extends Model {
-    _breakoutLevel: number;
+    @Model.exclude()
+    breakoutLevel: number;
+
+    @Model.exclude()
+    ship: any;
+
+    @Model.exclude()
+    stats: any;
+
+    @Model.exclude()
+    skin: any;
+
+    @Model.exclude()
+    group: any;
+
     id: number;
     name: string;
     code: number;
     type: number;
-    group: number;
     rarity: number;
     nation: string;
+    groupId: number;
     assetName: string;
     isResearch: boolean;
     hasRetrofit: boolean;
@@ -20,31 +34,29 @@ export default class ShipListItem extends Model {
         if (breakoutLevel > 4 || breakoutLevel < 1)
             throw new RequestError(400, "Breakout level should only be between 1 and 4.");
 
-        this.group = groupId;
-        this._breakoutLevel = breakoutLevel;
+        this.groupId = groupId;
+        this.breakoutLevel = breakoutLevel;
     }
 
     async load(ships : any[], shipGroups : any[], shipStats : any[], shipSkins : any[], shipRetrofits : any[], shipBlueprints : any[]): Promise<void> {
-        const group = shipGroups.find(g => g.group_type == this.group);
+        this.group = shipGroups.find(g => g.group_type == this.group);
 
-        if (!group)
+        if (!this.group)
             throw new RequestError(404, "Ship Group not found.");
 
-        const ship = ships.filter(s => s.group_type == group.group_type)[this._breakoutLevel - 1];
-        const stats = shipStats.find(s => s.id == ship.id);
-        const skin = shipSkins.find(s => s.id == stats.skin_id);
+        this.ship = ships.filter(s => s.group_type == this.group.group_type)[this.breakoutLevel - 1];
+        this.stats = shipStats.find(s => s.id == this.ship.id);
+        this.skin = shipSkins.find(s => s.id == this.stats.skin_id);
 
-        this.id = ship.id;
-        this.name = stats.name.trim();
-        this.code = group.code;
-        this.type = stats.type;
-        this.group = group.group_type;
-        this.rarity = stats.rarity;
-        this.nation = stats.nationality;
-        this.assetName = skin.painting;
-        this.isResearch = shipBlueprints.some(b => b.id == group.group_type);
-        this.hasRetrofit = shipRetrofits.some(r => r.group_id == group.group_type);
-
-        this.save({ ship, group, stats });
+        this.id = this.ship.id;
+        this.name = this.stats.name.trim();
+        this.code = this.group.code;
+        this.type = this.stats.type;
+        this.groupId = this.group.group_type;
+        this.rarity = this.stats.rarity;
+        this.nation = this.stats.nationality;
+        this.assetName = this.skin.painting;
+        this.isResearch = shipBlueprints.some(b => b.id == this.group.group_type);
+        this.hasRetrofit = shipRetrofits.some(r => r.group_id == this.group.group_type);
     }
 }
