5135336b370fbc23b5d615ec98d7f70a90abe7c7
Capuccino <chinodesuuu@gmail.com>
2020-09-25 02:23:01 +0000

    add cache-control headers
    Signed-off-by: Capuccino <chinodesuuu@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
+    async headers() {
+        return [{
+            source: "/api/*",
+            headers: [
+                {
+                    key: "cache-control",
+                    value: "s-maxage=201600, public"
+                }
+            ]
+        }];
+    },
     target: "serverless"
 };
