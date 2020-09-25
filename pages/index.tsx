fef404003142d112584a6b98d30cde98df3e123b
Capuccino <chinodesuuu@gmail.com>
2020-09-25 12:52:30 +0000

    use WebP version of Unicorn
    Signed-off-by: Capuccino <chinodesuuu@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
-                <img src="/unicorn.gif" height="200"></img>
+                <img src="/unicorn.webp" height="200"></img>
                 <h1 className={styles.title}>
                     Welcome to Unicorn!
                 </h1>
