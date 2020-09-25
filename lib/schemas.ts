12f4d611d6796a888fb0b603a1e1d5a923ae844f
Capuccino <chinodesuuu@gmail.com>
2020-09-25 00:58:58 +0000

    Finish writing initial schemas for ship endpoint
    Signed-off-by: Capuccino <chinodesuuu@gmail.com>

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -5,5 +5,20 @@ import Joi from "@hapi/joi";
  * if you're planning to check more than one argument, you will need a new Joi.object
  * to see what to expect in the request querystring.
  */
-export const LangQueryStringSchema = Joi.string().valid("en", "jp", "tw", "cn", "kr");
+export const langQueryStringSchema = Joi.string().valid("en", "jp", "tw", "cn", "kr");
 
+/**
+ * The expected values for the ship endpoint.
+ */
+export interface getShipQuery {
+    groupId?: number,
+    breakoutLevel?: number
+}
+
+/**
+ * Validation schema for the ship endpoint queries.
+ */
+export const getShipQuerySchema = Joi.object({
+    groupId: Joi.number().optional(),
+    breakoutLevel: Joi.number().optional()
+});
