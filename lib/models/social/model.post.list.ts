404719214ac78129e92af28919b5ebefc179bcd2
unknown <n20gaming2000@gmail.com>
2020-09-26 17:22:35 +0800

    create juustagram post models

---
+++ lib/models/social/model.post.list.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2019 - 2020 Nathan Alo, Ayane Satomi, et al.
+ * Licensed under the GNU General Public License v3
+ * See LICENSE for details.
+ */
+import Post from "./model.post";
+import ShareCfgModel from "../model.sharecfg.base";
+import ShareCfgModelList from "../model.sharecfg.list.base";
+
+@ShareCfgModel.dependsOn([ "social" ])
+export default class PostList extends ShareCfgModelList<Post> {
+    async load(posts: any[]) {
+        this.entries = posts.map(s => new Post(s.id));
+    }
+}
