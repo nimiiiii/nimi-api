ad26d7961658339615048489fc6b9bea8aacaa2b
unknown <n20gaming2000@gmail.com>
2020-09-26 19:26:58 +0800

    add item api

---
+++ pages/api/items/chat/index.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2019 - 2020 Nathan Alo, Ayane Satomi, et al.
+ * Licensed under the GNU General Public License v3
+ * See LICENSE for details.
+ */
+import ChatBubbleList from "lib/models/items/model.item.chat.list";
+import getResolver from "lib/getResolver";
+import methods from "lib/methods";
+
+export default methods({
+    get: async (_, res) =>
+        res.status(200).json(await new ChatBubbleList().run(await getResolver()))
+});
