d88207bc1604ccc92b39bd79a6a9afd4122f6aac
unknown <n20gaming2000@gmail.com>
2020-09-26 17:41:42 +0800

    add tasks api

---
+++ pages/api/tasks/[id].ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2019 - 2020 Nathan Alo, Ayane Satomi, et al.
+ * Licensed under the GNU General Public License v3
+ * See LICENSE for details.
+ */
+import Task from "lib/models/shared/model.task";
+import getResolver from "lib/getResolver";
+import methods from "lib/methods";
+import validate from "lib/validate";
+import { GetEntryByIdQuery, GetEntryByIdSchema } from "lib/schemas";
+
+export default methods({
+    get: validate<GetEntryByIdQuery, "query">(
+        { schema: GetEntryByIdSchema, location: "query" },
+        async (req, res) =>
+            res.status(200).json(
+                await new Task(req.body.id).run(await getResolver())
+            )
+    )
+});
