60ecf5cc220ae14a5e570715134a823e05d0ac5a
tommywalkie <tommywalkie@gmail.com>
2020-09-26 15:11:32 +0200

    Removed demonstration-purpose math test and unnecessary lines

--- tests/index.ts
+++ tests/index.ts
@@ -8,13 +8,7 @@ import { suite } from "uvu";
 const $: uvu.Test<Record<string, any>> = suite("sample test");
 
 // Setup process environment variables for uvu
-const config = dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
-process.env.GITHUB_REPO = config.parsed.GITHUB_REPO;
-process.env.GITHUB_TOKEN = config.parsed.GITHUB_TOKEN;
-
-$("should do some math", async () => {
-    is(1 + 4, 5);
-});
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
 
 $("should request some ship info", async () => {
     const ship = await new Ship(10701, 1).run(await getResolver());
