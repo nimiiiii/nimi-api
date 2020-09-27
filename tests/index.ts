5a54dbc3e757d2e1811b4db3120acc2b8706fc83
tommywalkie <tommywalkie@gmail.com>
2020-09-27 15:23:38 +0200

    Setup github actions for tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,14 +1,9 @@
 import Ship from "lib/models/ships/model.ship";
-import dotenv from "dotenv";
 import getResolver from "lib/getResolver";
 import { is } from "uvu/assert";
-import path from "path";
 import { suite } from "uvu";
 
 const $: uvu.Test<Record<string, any>> = suite("sample test");
-
-// Setup process environment variables for uvu
-dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
 
 $("should request some ship info", async () => {
     const ship = await new Ship(10701, 1).run(await getResolver());
