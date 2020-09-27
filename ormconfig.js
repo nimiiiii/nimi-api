028369b3ba1a9ac9e9399a670ee8e2c2a51772f9
Ayane Satomi <chinodesuuu@gmail.com>
2020-09-27 00:51:48 +0000

    add more properties to default connection
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

--- ormconfig.js
+++ ormconfig.js
@@ -6,7 +6,10 @@
 /* eslint-disable no-undef */
 module.exports = {
     type: "mongodb",
-    host: process.env.UNICORN_DB_HOST || "localhost",
-    entities: ["./entities/*.ts"],
-    migrations: ["./migrations/*.ts"]
+    host: process.env.UNICORN_DB_HOST || "mongodb://localhost:27017",
+    useNewUrlParser: true,
+    synchronize: true,
+    logging: true,
+    entities: ["./lib/entities/*.*"],
+    migrations: ["./lib/migrations/*.*"]
 };
