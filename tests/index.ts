7e42f13f39f3be2246f6c86d11e08071852c243e
Nitrous <n20gaming2000@gmail.com>
2021-01-21 13:33:40 +0800

    update tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,12 +1,16 @@
 import Ship from "lib/models/ships/model.ship";
 import { is } from "uvu/assert";
-import { suite } from "uvu";
+import { suite, uvu } from "uvu";
 
 const $: uvu.Test<Record<string, any>> = suite("sample test");
 
 $("should request some ship info", async () => {
-    const ship = await new Ship(10701, 1).run();
-    is(ship.name, "Langley");
+    const ship = new Ship(10701, 1);
+    ship.setResolverDefaults(process.env.GITHUB_TOKEN, process.env.REMOTE_REPO, "en");
+
+    await ship.run();
+
+    is((ship as any).name, "Langley");
 });
 
 $.run();
